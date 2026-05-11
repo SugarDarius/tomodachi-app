0d5ee47f12fb51b8b0b741c2e8f3407ef91a8916
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-05-11 14:16:34 +0200

    add realtime client

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { RealtimeProvider } from '@upstash/realtime/client'
+
 import { SidebarInset, SidebarProvider } from '~/components/ui/sidebar'
 import { ImportContactsProvider } from '~/components/contacts/import-contacts-provider'
 
@@ -6,13 +8,15 @@ import { AppSidebar } from './_components/app-sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-row h-screen w-screen'>
-      <SidebarProvider>
-        <ImportContactsProvider>
-          <AppSidebar getContactsListsPromise={getContactsLists()} />
-          <SidebarInset className='overflow-y-auto'>{children}</SidebarInset>
-        </ImportContactsProvider>
-      </SidebarProvider>
-    </div>
+    <RealtimeProvider>
+      <ImportContactsProvider>
+        <div className='flex flex-row h-screen w-screen'>
+          <SidebarProvider>
+            <AppSidebar getContactsListsPromise={getContactsLists()} />
+            <SidebarInset className='overflow-y-auto'>{children}</SidebarInset>
+          </SidebarProvider>
+        </div>
+      </ImportContactsProvider>
+    </RealtimeProvider>
   )
 }
