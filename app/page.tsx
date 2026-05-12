30a0aadfe05e4cf0ea9b0b5b5f14e5cfb767be45
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-05-12 17:05:55 +0200

    remove force dynamic directive

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { redirect } from 'next/navigation'
 import { auth } from '~/lib/auth/server'
 
-export const dynamic = 'force-dynamic'
-
 export default async function Page() {
   const { data: session } = await auth.getSession()
   if (!session) {
