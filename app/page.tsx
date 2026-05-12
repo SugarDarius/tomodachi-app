ca7bcae46fe12c10d2c8183861de8697d61e49a6
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-05-12 18:55:05 +0200

    simple redirect is enough

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from 'next/navigation'
-import { auth } from '~/lib/auth/server'
 
 export default async function Page() {
-  const { data: session } = await auth.getSession()
-  if (!session) {
-    redirect('/auth/sign-in')
-  }
-
   redirect('/dashboard')
 }
